b91c94d74f8bf95c36970e90a6eb4f9b04961825
Bruno <brunoassis2029@gmail.com>
2023-03-05 18:26:51 -0300

    metodo de pesquisa

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
+import react from "react"
 import config from "../config.json"
 import styled from "styled-components"
 import { CSSReset } from "../src/components/reset"
-import Menu from "../src/components/Menu"
+import Menu from "../src/components/Menu/index"
 import { StyledTimeline } from "../src/components/Timeline";
+
 
 
 
 function HomePage() {
     
     const estiloDaHomePage ={
-        // backgroundColor: "red"
+
     }
+    const [valorDoFiter,setValorDofilter] = react.useState('');
+    // const valorDoFiter = "PR"
     return (
         <> 
         <CSSReset/>
         <div style={estiloDaHomePage}>
-        <Menu/>
+        <Menu valorDoFltro={valorDoFiter} setValorDofilter={setValorDofilter} />
         <Header/>
-        <TimeLine playlists={config.playlists}/>
+        <TimeLine searchValue={valorDoFiter} playlists={config.playlists}/>
         </div>
         
     </>
@@ -47,15 +51,23 @@ const StyledHeader = styled.div`
         padding: 16px 32px;
         gap: 16px;
     }
-    .bunner{
-        max-height: 300px;
-        width: 100%;
-        height: 50%;
-    }
 `;
+const StyledBanner = styled.div`
+     width: 100%;
+     background-image: url("https://images.unsplash.com/photo-1614583225154-5fcdda07019e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=890&q=80");
+     background-position: top;
+     background-size: cover;
+     background-repeat: no-repeat;
+     height: 50vh;
+     max-height: 380px;
+`;
+
+
+
 function Header(){
     return(
         <StyledHeader>
+           <StyledBanner/>
            <img className="bunner" src={config.bunner}/>
            <section className="user-info">
                <img src={`https://github.com//${config.github}.png`}/>
@@ -72,7 +84,7 @@ function Header(){
         </StyledHeader>
     )
 }
-function TimeLine(props){
+function TimeLine({searchValue, ...props}){
     
     const playlistNames = Object.keys(props.playlists);
     return(
@@ -81,13 +93,17 @@ function TimeLine(props){
                 const videos = props.playlists[PlayListName];
                 
                 return (
-                    <section>
+                    <section key={playlistNames}>
                         <h2>{PlayListName}</h2>
                         <div>
-                            {videos.map((video)=>{
+                         {videos.filter((video)=>{
+                            const titleNormalizerd= video.title.toLowerCase()
+                            const searchValuetitleNormalizerd= searchValue.toLowerCase()
+                            return titleNormalizerd.includes(searchValuetitleNormalizerd)
+                         }).map((video)=>{
                     
                     return (
-                        <a href={video.url} target="_blank">
+                        <a key={video.url} href={video.url} target="_blank">
                           <img src={video.thumb}/>
                           <span>
                           {video.title}
---
+++ src/components/Menu/components/Search.js
@@ -0,0 +1,50 @@
+import react from "react";
+import React from "react";
+import styled from "styled-components"
+
+const StyledSearch = styled.div`
+  display: flex;
+  flex-direction: row;
+  border: 1px solid ${({ theme }) => theme.borderBase};
+  max-width: 425px;
+  width: 100%;
+  border-radius: 2px;
+  overflow: hidden;
+  
+  input {
+    width: 80%;
+    padding: 4px 6px;
+    border: none;
+    outline: none;
+    color: ${({ theme }) => theme.textColorBase};
+    background-color: ${({ theme }) => theme.backgroundBase};
+  }
+  button {
+    flex: 1;
+    cursor: pointer;
+    border: none;
+    background-color: ${({ theme }) => theme.backgroundLevel2};
+    box-shadow: 0 1px 0 rgb(0 0 0 / 10%);
+    border-left: 1px solid ${({ theme }) => theme.borderBase};
+    width: 40px;
+    height: 40px;
+    @media (min-width: 600px) {
+      width: 64px;
+      height: 40px;
+    }
+  }
+`;
+export default function Search({valorDoFiter,setValorDofilter}){
+    // const [valorDaBusca,setValorDaBusa] = react.useState('Frost');
+    // console.log("Pau no",valorDaBusca)
+    const valorDoBusca = valorDoFiter
+    const setValorDaBusca = setValorDofilter
+    return(
+      <StyledSearch>
+        <input type="text" onChange={(e)=>setValorDaBusca(e.target.value)} value={valorDoBusca}/> 
+            <button>
+                🔎
+            </button>
+      </StyledSearch>
+    )
+}
